5d8f11b9859087ee1b64b3074365a45449ac1f16
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2014-09-08 16:57:05 -0400

    Global and Local scope test

--- js/site.js
+++ js/site.js
@@ -1,4 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var tagline = document.querySelector("p.tagline");
-    tagline.innerText = "From Cloud9 IDE!";
-});
+var values = [1, 2, 3, 4, 5];
+
+var myStore = [{category: "Cat A"}, {category: "Cat B"}, {category: "Cat A"}];
+
+var mySimpleStore = ["Cat A", "Cat B", "Cat A"];
+
+window.color = "window Color"
+var myObject = { color: "object Color"};
+
+var getColor = function(){
+	alert(this.color);
+};
+
+// Window level
+getColor();
+
+getColor.apply(this);
+getColor.apply(window);
+getColor.apply(myObject)
