f67048ca9f56fea926ad9ba72fc0d18607166791
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2014-09-09 00:03:02 -0400

    Casting function vs New operator

--- js/site.js
+++ js/site.js
@@ -1,19 +1,6 @@
-var values = [1, 2, 3, 4, 5];
+var intAsString = "123";
+var intValue = Number(intAsString);		// intValue is of type Number
+alert("Original: " + typeof intAsString + " \nUpdated type: " + typeof intValue);
 
-var myStore = [{category: "Cat A"}, {category: "Cat B"}, {category: "Cat A"}];
-
-var mySimpleStore = ["Cat A", "Cat B", "Cat A"];
-
-window.color = "window Color"
-var myObject = { color: "object Color"};
-
-var getColor = function(){
-	alert(this.color);
-};
-
-// Window level
-getColor();
-
-getColor.apply(this);
-getColor.apply(window);
-getColor.apply(myObject)
+var obj = new Number(intAsString);	// obj is of type Object
+alert("Original: " + typeof intAsString + " \nUpdated type: " + typeof obj);
