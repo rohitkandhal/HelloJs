0b7b004ddeff0440f744d0fd7e996d86314d8941
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2014-09-11 16:45:40 -0400

    Constructor Pattern to create object

--- js/site.js
+++ js/site.js
@@ -1,39 +1,22 @@
-var company = {};
+//Constructor - Global Scope
+function Company(name, year){
+	this.name = name;
+	this._year = year;
 
-Object.defineProperties(company, {
-	_name : {
-		// Note that if you don't make it writable specifically then it would be read only
-		writable: true,
-		value: "Oil Systems Inc"
-	},
-
-	name: {
-		configurable: true,
-		get: function(){
-			return this._name;
+	Object.defineProperties(this, {
+		year: {
+			get: function(){
+				return this._year;
+			},
+			set: function(newValue){
+				if(Number(newValue)){				
+					this._year = newValue;
+				}
+			}
 		}
-	}
-});
+	});
+}
 
-/*
-Defined 
-	1 Data Properties: _name
-	1 Accessor Properties: name (with getter only)
-*/
+var myCompany = new Company("My Company", 056 );
 
-alert(company.name);	// "Oil Systems Inc"
-
-// try to change the property. It shouldn't change
-company.name = "nagarro";
-alert(company.name);	// "Oil SYstems Inc"
-
-// Add/update name property with setter
-Object.defineProperty(company, "name", {
-	set: function(newValue){
-		this._name = newValue;
-	}
-});
-
-// Now try to update it.
-company.name = "nagarro";
-alert(company.name);	// "nagarro"
+alert(myCompany.name + ", " + myCompany.year);	// Output: My Company, 46
