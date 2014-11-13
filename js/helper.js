e451ac344a3f31d388ae430015f0313f31a945a7
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2014-11-13 11:21:32 -0500

    Added JavaScript Helper file

---
+++ js/helper.js
@@ -0,0 +1,15 @@
+// ----------------------- Even Odd check ----------
+function isNumber(n)
+{
+   return n == parseFloat(n);
+}
+
+function isEven(n) 
+{
+   return isNumber(n) && (n % 2 == 0);
+}
+
+function isOdd(n)
+{
+   return isNumber(n) && (Math.abs(n) % 2 == 1);
+}
