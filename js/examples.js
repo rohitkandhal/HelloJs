10721f3ad1253a093789a78282c8dd520fd8a2d9
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2015-05-05 23:19:26 -0400

    Added CSV reader

--- js/examples.js
+++ js/examples.js
@@ -1,13 +1,18 @@
 // 1. CSV Reader
 
-function CSVReader(separators){
-    this.separators = separators || [","];
-
-    this.regex = new RegExp(this.separators.map(function(item) {
-        return "\\" | item[0];
-    }).join("|"));
+function CSVReader(){
+    this.regex = /,/; // use comma as delimiter
 }
 
 CSVReader.prototype.read = function(inp){
   var lines = inp.trim().split(/\n/);
+  
+  return lines.map(function(line) {
+    return line.split(this.regex);
+  }, this); // for this.regex we need to specifically send reference of CSVReader
+  
+  // Also we can save this as var self = this and use self.regex
 };
+
+var reader = new CSVReader();
+console.table(reader.read("a,b,c\nd,e,f\n"));
