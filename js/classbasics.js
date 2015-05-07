ef7c7a4d3b384b1681c0086a39d782f866683e0f
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2015-05-07 14:51:04 -0400

    Inheritance example added

--- js/classbasics.js
+++ js/classbasics.js
@@ -111,3 +111,85 @@ var makeCounter = function() {
 // Both counter will work independently
 var counter1 = makeCounter();
 var counter2 = makeCounter();
+
+
+// INHERITANCE
+function Scene(context, images, height, width){
+    this.context = context;
+    this.images = images;
+    this.height = height;
+    this.width = width;
+    this.actors = [];
+}
+
+Scene.prototype.register = function(actor){
+    this.actors.push(actor);
+}
+
+Scene.prototype.unregister = function(actor){
+    var index = this.actors.indexOf(actor);
+    if(index >=0){
+        this.actors.splice(index, 1);
+    }
+}
+
+Scene.prototype.draw = function() {
+    this.context.clearRect(0, 0, this.width, this.height);
+
+    for ( var a = this.actors, i = 0, n = a.length; i < n; i++){
+        a[i].draw();
+    }
+};
+
+
+function Actor(scene, x, y){
+    this.scene = scene;
+    this.x = x;
+    this.y = y;
+    scene.register(this);
+}
+
+Actor.prototype.moveTo = function(x, y){
+    this.x = x;
+    this.y = y;
+    this.scene.draw();
+}
+
+Actor.prototype.exit = function() {
+    this.scene.unregister(this);
+    this.scene.draw();
+}
+
+Actor.prototype.draw = function() {
+    var image = this.scene.images[this.type];
+    this.scene.context.drawImage(image, this.x, this.y);
+};
+
+Actor.prototype.width = function(){
+    return this.scene.images[this.type].width;
+};
+
+Actor.prototype.height = function() {
+    return this.scene.images[this.type].height;
+};
+
+function SpaceShip(scene, x, y){
+    Actor.call(this, scene, x, y);
+    this.points = 0;
+};
+
+SpaceShip.prototype = Object.create(Actor.prototype);
+SpaceShip.prototype.type = "spaceShip";
+
+SpaceShip.prototype.scorePoint = function() {
+    this.points++;
+}
+
+SpaceShip.prototype.left = function() {
+    this.moveTo(Math.max(this.x -10 ,0), this.y);
+}
+
+SpaceShip.prototype.right = function() {
+    var maxWidth = this.scene.width - this.width();
+    this.moveTo(Math.min(this.x + 10, maxWidth), this.y);
+}
