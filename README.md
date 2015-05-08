185df09dcc5043a3ad0e071e327e4bdd54009893
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2015-05-08 03:17:23 -0400

    Added Arrays and Dictionary examples

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ##Hello JS  
 
 1. For Objects, Classes and Prototypes, see [classbasics.js](js/classbasics.js)
-2. There is no restriction on number of arguments being passed. _e.g. You can declare a function with no arguments but still you can send as many arguments you like. Arguments are accessible via **arguments** object._
+2. For Arrays, Dictionaries, see [arrays.js](js/arrays.js)
 3. **No Overloading**: If two functions are defined to have the same name, it is the last function that becomes the owner of that name. Tip: Use arguments.length to support Method Overloading.
 4. Unlike other programming languages, **string** is a primitive data type instead of a reference data type. 
 5. Only _reference values_ can have properties defined dynamically for later use.
@@ -17,3 +17,5 @@ var person = new Object();
 document.write(person.lastName);    // Output: Undefined
 addName(person);
 document.write(person.lastName);   // Output: Kandhal
+
+There is no restriction on number of arguments being passed. _e.g. You can declare a function with no arguments but still you can send as many arguments you like. Arguments are accessible via **arguments** object._
--- js/arrays.js
+++ js/arrays.js
@@ -15,3 +15,78 @@ var a2 = Array.apply(null, new Array(3));
 a2.forEach(function(x,i) {
     console.log(i+ ". " + x);
 })
+
+// 2.1 Hashmaps / Dictionary using RAW Object (Table of string - value)
+var dict1 = { 
+    rohit: 26,
+    karan: 25
+}
+
+    // for..in loop gives property names (both object and its prototype)
+for(var i in dict1){
+    console.log(i + ": " + dict1[i]);
+}
+
+// Only use direct instances of Object as dictionaries - not subclasses or arrays 
+
+// DON'T USE FOLLOWING because of PROTOTYPE Pollution
+var dict2 = {};
+dict2.rohit = 26;
+dict2.karan = 25;
+
+Object.prototype.count = function() {
+    var count = 0;
+    for(var item in this){
+        count++;
+    }
+    return count;
+}
+
+for(var i in dict2){
+    console.log(i + ": " + dict2[i]);
+}
+
+dict2.count();  // 3 though we've only 2 enteries
+
+// 2.2 Never add properties to Object.prototype, otherwise for..in loop wont work
+
+// 3.1 To avoid prototype pollution, use null prototype
+function C(){}
+C.prototype = null; // DOESN'T WORK instead use Object.Create
+
+var x = Object.create(null);    // Object.create(prototype, properties object)
+
+// 3.2 To avoid prototype pollution, use hasOwnProperty
+var hasOwn = Object.prototype.hasOwnProperty;
+
+var dict = {};
+hasOwn.call(dict, "hasOwnProperty");   // false
+dict.hasOwnProperty = 25;       // Beware someone might override the object function
+hasOwn.call(dict, "hasOwnProperty");   // true
+
+// 3.3 Good Custom Dictionary
+function Dict(elements) {
+    this.elements = elements || {};
+}
+
+Dict.prototype.add = function(key, val){
+    this.elements[key] = val;
+}
+
+Dict.prototype.remove = function(key){
+    if(this.has(key)){
+        // delete is only effective on an object's properties. It has no effect on variable or function names.
+        delete this.elements[key];
+    }
+}
+
+Dict.prototype.get = function(key){
+    // check for own property
+    return this.has(key) ? this.elements[key] : undefined; 
+}
+
+// HasOwnProperty or Key checker
+Dict.prototype.has = function(key){
+    return {}.hasOwnProperty.call(this.elements, key);
+}
+
