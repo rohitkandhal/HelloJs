874cef57cb7e731088179af6a2622d045e4192e4
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2015-05-13 16:03:53 -0400

    String to array conversion added

--- js/stringbasics.js
+++ js/stringbasics.js
@@ -1,2 +1,7 @@
 var s_primitive = 'foo';    // String Literal
 var s_obj = new String(s_prim); // String Object
+
+// 1. String to array conversion
+var str1 = "1,2";
+var arr1 = JSON.parse("[" + str1 + "]");    // arr1 = [1,2]
+var arr2 = str1.split(",");     // arr2 = ["1", "2"]
