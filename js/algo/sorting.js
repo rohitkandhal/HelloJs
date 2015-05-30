6b585aff5dac06327781805fd27259efedc683da
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2015-05-30 14:09:09 -0400

    Added heapsort

--- js/algo/sorting.js
+++ js/algo/sorting.js
@@ -6,6 +6,8 @@ window.sort = window.sort || {};
     ns.insertionSort = insertionSort;
     ns.mergeSort = mergeSort;
     ns.heapSort = heapSort;
+    ns.isHeap = isHeap;
+
     // Internal
 
     // 1. Insertion Sort algo
@@ -69,51 +71,99 @@ window.sort = window.sort || {};
 
     // 3. Heap sort
     function heapSort(inp) {
-        // Heap indexes start with 1
-        inp.unshift(0);
-        return heapify(inp, 2);
+        var heapSize = inp.length;
+        
+        buildHeap(inp, inp.length);
+        for (var i = 0; i < inp.length-1; i++) {
+            swapArrItems(inp, 0, heapSize - 1);
+            // console.log("Move root to end", inp);
+            heapSize--;
+            heapify(inp, 0, heapSize);
+            // console.log("Heapify: ", inp);
+        }
+        return inp;
     }
 
-    // Fixes heap
-    function heapify(arr, root) {
+    // Part 2: 
+    function buildHeap(inp, heapSize) {
+        for (var i = (heapSize / 2) - 1; i >= 0; i--) {
+            heapify(inp, i, heapSize);
+        }
+        return inp;
+    }
+
+    // Part 3: Puts arr[root] to right place in heap
+    // NOTE: all heap algo assume index to start from 1 but this is 
+    // implemented using 0 - arr based element index
+    function heapify(arr, root, heapSize) {
         var left, right, largest;
         
-        left = 2 * root;
-        right = 2 * root + 1;
+        if (!heapSize) {
+            heapSize = arr.length;
+        }
         
-        if (arr[left] && arr[left] > arr[root]) {
+        left = 2 * root + 1; // assuming root is 0 based index
+        right = 2 * root + 2;
+        
+        if (left < heapSize && arr[left] > arr[root]) {
             largest = left;
         } else {
             largest = root;
         }
         
-        if (arr[right] && arr[right] > arr[largest]) {
+        if (right < heapSize && arr[right] > arr[largest]) {
             largest = right;
         }
         
         if (largest !== root) {
             swapArrItems(arr, largest, root);
-            return heapify(arr, largest)
+            return heapify(arr, largest, heapSize)
         }
         return arr;
+    }
+
+    // Part 4: Verifies if an array is max heap
+    function isHeap(inp) {
+        for (var i = (inp.length / 2 - 1); i >= 0; i--) {
+            // root should be greater than both left and right node
+            if (inp[i] < inp[leftHeapNode(i)] || 
+            inp[i] < inp[rightHeapNode(i)]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    function leftHeapNode(i) {
+        return 2 * i + 1;
+    }
+    
+    function rightHeapNode(i) {
+        return 2 * i + 2;
     }
     
     function swapArrItems(arr, source, target) {
         var temp;
-        if(arr[source] && arr[target]) {
+        if (arr[source] && arr[target]) {
             temp = arr[source];
             arr[source] = arr[target];
-            arr[target] = arr[source];
+            arr[target] = temp;
         }
-        return swapArrItems;
+        return arr;
     }
 
 })(window.sort);
 
-
 var testArr = [5, 2, 4, 6, 1, 3];
+var testArrEmpty = [];
+var testArrSame = [5, 5, 5];
 var testHeap = [16, 4, 10, 14, 7, 9, 3, 2, 8, 1];
+var testHeapBuild = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7];
+var testHeapValid = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1];
+var testHeap2 = [23, 17, 14, 6, 13, 10, 1, 5, 7, 12];
 
 //window.sort.insertionSort(testArr);
 //window.sort.mergeSort(testArr, 0, testArr.length);
-window.sort.heapSort(testHeap);
+window.sort.heapSort(testArr);
+//window.sort.isHeap(testHeapValid);
+
