20ff2362fc3678f170c0ae3b15240d041409c786
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2015-06-03 02:42:14 -0400

    Added binary tree, test tree, and inorder traversal

---
+++ js/algo/tree.js
@@ -0,0 +1,79 @@
+// Binary Search Tree
+/* Test tree
+          15
+        /    \   
+       /      \
+      6         18
+    /  \      /    \
+  3     7    17     20
+ / \     \
+2   4     13
+         /
+        9
+*/
+
+window.tree = window.tree || {};
+
+(function(ns) {
+    
+    ns.BST = BST;
+
+    function BST(data, left, right, parent) {
+        this.data = data || undefined;
+        this.left = left || undefined;
+        this.right = right || undefined;
+        this.parent = parent || undefined;
+    }
+
+    // Inorder traversal - recursion
+    var inorderTraverse = function(root) {
+        var out = '';
+        if(root != null) {
+            out += inorderTraverse(root.left);
+            out += root.data + " ";
+            out += inorderTraverse(root.right);
+        }
+        return out;
+    }
+
+    var getTestTree = function() {
+        var n2, n3, n4, n6, n7, n9, n13, n15, n17, n18, n20;
+
+        n2 = new BST(2);
+        n3 = new BST(3);
+        n4 = new BST(4);
+        n6 = new BST(6);
+        n7 = new BST(7);
+        n9 = new BST(9);
+        n13 = new BST(13);
+        n15 = new BST(15);
+        n17 = new BST(17);
+        n18 = new BST(18);
+        n20 = new BST(20);
+
+        n2.parent = n4.parent = n3;
+        n9.parent = n13;
+        n13.parent = n7;
+        n3.parent = n7.parent = n6;
+        n17.parent = n20.parent = n18;
+        n6.parent = n18.parent = n15;
+
+        n15.left = n6; n15.right = n18;
+        n6.left = n3; n6.right = n7;
+        n3.left = n2; n3.right = n4;
+        n7.right = n13;
+        n13.left = n9;
+        n18.left = n17; n18.right = n20;
+     
+        return n15;   
+    };
+
+    BST.prototype.getTestTree = getTestTree;
+    BST.prototype.inorderTraverse = inorderTraverse;
+
+}(window.tree))
+
+// test
+var bst = new window.tree.BST();
+var testTree = bst.getTestTree();
+bst.inorderTraverse(testTree);
