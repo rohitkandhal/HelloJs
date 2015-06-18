d510aa1badea44f7d8a9c999060aa99b76447b13
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2015-06-18 17:00:28 -0400

    pri Q partial

--- js/priorityqueue.js
+++ js/priorityqueue.js
@@ -33,13 +33,50 @@ window.ds = window.ds || {};
             }
         }
     });
-    
+
     // 3|6|7|11|13|15|18
     // 0|1|2| 3| 4| 5| 6
     PriorityQueue.prototype.buildHeap = function() {
         var i;
-        for(i = (this.data.heapSize/2).toFixed(0) - 1; i >= 0; i--){
-            
+        for (i = (this.data.heapSize / 2).toFixed(0) - 1; i >= 0; i--) {
+            minHeapify(this.data, i);
         }
+    }
+    
+    function minHeapify(heap, id) {
+        var smallest, l, r;
+        if (heap) {
+            l = leftChild(i);
+            r = rightChild(i);
+            
+            if (l < heap.heapSize && heap[l] < heap[id]) {
+                smallest = l;
+            } else {
+                smallest = id;
+            }
+            
+            if (r < heap.heapSize && heap[r] < heap[smallest]) {
+                smallest = r;
+            }
+            
+            if (smallest !== id) {
+                swap(heap, smallest, id);
+                minHeapify(smallest);
+            }
+        }
+    }
+    
+    function swap(heap, item1, item2) {
+        var temp = heap[item1];
+        heap[item1] = heap[item2];
+        heap[item2] = temp;
+        return temp;
+    }
+    
+    function leftChild(i) {
+        return 2 * i + 1;
+    }
+    function rightChild(i) {
+        return 2 * i + 2;
     }
 }(window.ds));
