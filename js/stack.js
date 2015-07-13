d0b9245f0fb9179a1673a463f41d18c70aeacaf8
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2015-07-13 10:53:37 -0400

    Added queue implementation

--- js/stack.js
+++ js/stack.js
@@ -1,9 +1,11 @@
 
+// Linked List
 function Node(data) {
     this.data = data;
     this.next = null;
 }
 
+// Stack implemented using LinkedList
 function Stack() {
     this.top = null;
 }
@@ -32,8 +34,52 @@ Stack.prototype.print = function() {
     }
 }
 
-var stack = new Stack();
-stack.push(3);
-stack.push(5);
-stack.push(7);
-stack.print();
+// var stack = new Stack();
+// stack.push(3);
+// stack.push(5);
+// stack.push(7);
+// stack.print();
+
+// Queue implemented using LinkedList
+function Queue() {
+    this.head = null;
+    this.tail = null;
+}
+
+Queue.prototype.enqueue = function (data) {
+    var newNode = new Node(data);
+
+    if(this.head === null) {
+        this.head = newNode;
+        this.tail = newNode;
+    } else {
+        this.tail.next = newNode;
+        this.tail = newNode;
+    }
+}
+
+Queue.prototype.dequeue = function() {
+    var newNode;
+    if(this.head !== null) {
+        newNode = this.head.data;
+        this.head = this.head.next;
+    }
+    return newNode;
+}
+
+Queue.prototype.print = function() {
+    var curr = this.head;
+    while(curr) {
+        console.log(curr.data);
+        curr = curr.next;
+    }
+}
+
+var queue = new Queue();
+queue.enqueue(3);
+queue.enqueue(5);
+queue.enqueue(7);
+queue.print();
+queue.dequeue();
+queue.dequeue();
+queue.print();
