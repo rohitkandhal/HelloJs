182038394294aabbf9e1c7a4d93a933eedb01cb5
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2015-08-18 10:50:55 -0400

    linked list restructured

--- js/algo/linkedlist.js
+++ js/algo/linkedlist.js
@@ -1,49 +1,81 @@
- // 1. Linked list implementation
+// 1. Linked list implementation
 
 window.ds = window.ds || {};
 
 (function(ns) {
-    
     ns.LinkedList = LinkedList;
-    ns.createListFromArr = createListFromArr;
-    ns.printList = printList;
-
-    // 
-    function LinkedList(data, next) {
+    
+    function LinkedList() {
+        this.head;
+    }
+    
+    // Linked List node
+    function Node(data, next) {
         this.data = data;
         this.next = next;
     }
     
-    function createListFromArr(arr) {
-        var head = new LinkedList();
-        var curr = head;
+    // Prototype functions
+    LinkedList.prototype.append = append;
+    LinkedList.prototype.insert = insert;
+    LinkedList.prototype.print = printList;
+    
+    // Function implementations
+    
+    function append(inp) {
+        // Create a list from an array
+        if (Array.isArray(inp)) {
+            for (var i = 0; i < inp.length; i++) {
+                this.insert(inp[i]);
+            }
+            
+            // inp.forEach(function(key) {
+            //  this.insert(key);   // ERROR: this scope is not linkedlist
+            // });
+        }
+    }
+    
+    function insert(data) {
+        var curr, 
+        newNode;
         
-        arr.forEach(function(x) {
-            var newNode = new LinkedList(x);
+        curr = this.head;
+        newNode = new Node(data);
+        
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            while (curr && curr.next) {
+                curr = curr.next;
+            }
             curr.next = newNode;
-            curr = curr.next;
-        });
+        }
         
-        return head.next;   // careful: we don't do any manipulation at head
+        return this.head;
     }
     
     function printList(head) {
-        var curr = head;
-        var output = "";
-        while(curr !== undefined) {
-            output += (curr.data + " ");
+        var curr = head
+          , 
+        out = [];
+        
+        curr = curr || this.head;
+        while (curr) {
+            out.push(curr.data);
             curr = curr.next;
         }
-        return output.trim();
+        
+        return out.join(' -> ');
     }
+}
+(window.ds));
 
-}(window.ds));
-
-// test functions
+// testing linked list
 var testArr = [5, 2, 4, 6, 1, 3];
 var testArrEmpty = [];
 var testArrSame = [5, 5, 5];
 
-var head1 = window.ds.createListFromArr(testArrEmpty);
-window.ds.printList(head1);
-
+var l1 = new window.ds.LinkedList();
+l1.append(testArr);
+l1.append(testArrSame);
+l1.print();
