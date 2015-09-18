ea8387eaeba5eda3a6edea372da6923a70dc4be3
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2015-09-18 18:27:32 -0400

    graph algo bfs

--- js/algo/graph.js
+++ js/algo/graph.js
@@ -7,29 +7,96 @@
 window.ds = window.ds || {};
 
 (function(ns) {
+	ns.Graph = Graph;
+	
 	function Graph() {
-		this.nodes = {};	// Node -> [adj List array]
+		this.vertices = [];
 	}
 	
-	Graph.prototype.addNode = addNode;
-	Graph.prototype.addEdge = addEdge;
+	function Vertex(data) {
+		this.data = data;
+		this.visited = false;
+		this.distance = 0;
+		this.edges = [];
+	}
 	
-	function addNode(node) {
-		if(!this.nodes[node]) {
-			this.nodes[node] = [];
+	Graph.prototype.addVertex = addVertex;
+	Graph.prototype.addEdge = addEdge;
+	Graph.prototype.getVertex = getVertex;
+	Graph.prototype.toString = toString;
+	
+	function addVertex(data) {
+		var v = new Vertex(data)
+		this.vertices.push(v);
+	}
+	
+	function getVertex(data) {
+		var i, curr;
+		
+		for(i = 0; i < this.vertices.length; i++) {
+			curr = this.vertices[i];
+			
+			if(curr.data === data) {
+				return curr;
+			}
 		}
 	}
 	
-	function addEdge(n1, n2) {
-		this.nodes[n1].push(n2);
-		this.nodes[n2].push(n1);
+	function addEdge(d1, d2) {
+		var v1 = this.getVertex(d1),
+			v2 = this.getVertex(d2);
+		
+		v1.edges.push(v2.data);
+		v2.edges.push(v1.data);
+	}
+	
+	function bfs(source){
+		var v = this.vertices, 
+		adjList = this.adjList,
+		curr,
+		neighbors,
+		queue = [];
+		
+		queue.push(source);
+		
+		while(queue.length > 0) {
+			curr = queue.shift();
+			neighbors = curr.edges;
+			
+		}
+		
 	}
 	
 	function toString() {
-		var out = '', i;
+		var out = '', i, v;
 		
-		for(i = 0; i < this.nodes)
+		for(i = 0; i < this.vertices.length; i += 1) {
+			v = this.vertices[i];
+			
+			out += v.data + ' -> ';
+			out += v.edges.join(', ');
+			out += '\n';
+		}
+		return out;
 	}
 	
-	
 }(window.ds));
+
+// test
+var graph = new window.ds.Graph();
+var myVertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+myVertices.forEach(function(v) {
+	graph.addVertex.call(graph, v);
+	});
+	
+graph.addEdge('A', 'C');
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'D');
+graph.addEdge('C', 'D');
+graph.addEdge('D', 'G');
+graph.addEdge('D', 'H');
+graph.addEdge('B', 'E');
+graph.addEdge('B', 'F');
+// graph.addEdge('E', 'I'); disconnect I
+
+graph.toString();
